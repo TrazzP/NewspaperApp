d296be6fdc5158355e2383bc849b0fc0b83ad907
Trazz <trazz.pepper@umconnect.umt.edu>
2023-10-26 14:01:13 -0600

    started the news posts

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -39,12 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "accounts",
     "pages",
     "articles",
-    # 3rd Party
-    "crispy_forms",
-    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -113,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "America/Denver"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
