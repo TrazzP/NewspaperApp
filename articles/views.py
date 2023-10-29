b6305aebfb8b341866d9bbd8e0a7b9e1452a3599
Trazz <trazz.pepper@umconnect.umt.edu>
2023-10-29 15:16:44 -0600

    finished the permissons and autorizations

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,45 @@
-from django.views.generic import ListView
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Article
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = (
+        "title",
+        "body",
+    )
+    template_name = "article_edit.html"
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = ("title", "body")
+    login_url = "login"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
