667edf9583c7a864c2989f32f48f7ba6c4a026a5
Trazz <trazz.pepper@umconnect.umt.edu>
2023-10-31 11:12:35 -0600

    final password reset visability

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,3 @@
-from django.forms import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
@@ -7,10 +6,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
