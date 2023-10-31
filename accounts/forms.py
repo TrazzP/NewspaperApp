17cb9a2c7e1078b202b7f269296fa56dd448710e
Trazz <trazz.pepper@umconnect.umt.edu>
2023-10-31 11:23:12 -0600

    made email mandatory

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,14 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].required = True
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = (
